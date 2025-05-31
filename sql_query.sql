119a4a57a323ca849a39f6c8d3dba4b1a3da4466
NassimBahlouli <mednassimbahlouli@gmail.com>
2025-06-01 01:53:06 +0200

    Create sql_query.sql

---
+++ sql_query.sql
@@ -0,0 +1,11 @@
+-- Ce fichier comporte toutes les commandes SQL utilisées --
+
+
+-- 1) Création de la base de données NETFLIX
+CREATE DATABASE NETFLIX;
+
+--2) Executer une requete pour voir l’ensemble de données et comprendre sa structure et son contenu. ( j'ai affiché une seule ligne )
+SELECT * FROM netflix_rawn LIMIT 1;
+
+
+
